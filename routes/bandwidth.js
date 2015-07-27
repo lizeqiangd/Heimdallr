89e41f035f949ba08c92e0c8430530cefa8dff5d
Lizeqiangd <Lizeqiangd@gmail.com>
2015-07-27 18:00:40 +0800

    update api to object mode

--- routes/bandwidth.js
+++ routes/bandwidth.js
@@ -4,37 +4,26 @@
  * GET online page.
  */
 var os = require('os');
-var data = [];
-
 var exec  = require('child_process').exec;
 var order = 'netstat -n -i | awk \'{if ($1 > 0 && $4 > 0) print $1","$4","$8}\' | grep -i -v name | uniq';
+
+var data = [];
 var RX    = 0;
 var TX    = 0;
-exec(order,
-    function (error, stdout, stderr) {
-        var item = stdout.toString().split('\n');
-        for (var i = item.length - 2; i >= 1; i--) {
-            var ps = item[i].split(',');
-            RX += parseInt(ps[1]);
-            TX += parseInt(ps[2]);
-        }
-        data = [RX, TX];
-        if (error !== null) {
-            console.log('exec error: ' + error);
-        }
-    }
-);
-
+var delta_RX=0;
+var delta_TX=0;
 exports.main = function(req, res){
     exec(order,
         function (error, stdout, stderr) {
             var item = stdout.toString().split('\n');
             for (var i = item.length - 2; i >= 1; i--) {
                 var ps = item[i].split(',');
-                RX += parseInt(ps[1]);
-                TX += parseInt(ps[2]);
+                delta_RX=parseInt(ps[1])-RX;
+                delta_TX=parseInt(ps[2])-TX;
+                RX = parseInt(ps[1]);
+                TX = parseInt(ps[2]);
             }
-            data = [RX, TX];
+            data = [RX, TX];            
             if (error !== null) {
                 console.log('exec error: ' + error);
             }
--- routes/df.js
+++ routes/df.js
@@ -1,37 +1,41 @@
 /*
- * GET diskfile page.
+ * GET diskfile page. 
+ * /bin/df |awk '{print $1","$2","$3","$4","$5","$6}'
+ * @edit by:Lizeqiangd.
  */
 var os = require('os');
-var data = [];
-
 var exec = require('child_process').exec;
-exec('/bin/df -h|awk \'{print $1","$2","$3","$4","$5","$6","$7","$8","$9}\'',
-    function (error, stdout, stderr) {
-        var item = stdout.toString().split('\n');
-        for (var i = item.length - 1; i >= 1; i--) {
-            var disk = item[i].split(',');
-            var tmp = [disk[0], disk[1], disk[2], disk[3], disk[4], disk[disk.length-1]];
-            data.push(tmp);
-        }
-        if (error !== null) {
-            console.log('exec error: ' + error);
-        }
-    }
-);
-
+var rmsg = require('../api/utils.js').get_return_object;
 exports.main = function(req, res){
-    exec('/bin/df -h|awk \'{print $1","$2","$3","$4","$5","$6","$7","$8","$9}\'',
+    exec('/bin/df |awk \'{print $1","$2","$3","$4","$5","$6}\'',
         function (error, stdout, stderr) {
-            var item = stdout.toString().split('\n');
-            for (var i = item.length - 1; i >= 1; i--) {
-                var disk = item[i].split(',');
-                var tmp = [disk[0], disk[1], disk[2], disk[3], disk[4], disk[disk.length-1]];
-                data.push(tmp);
-            }
             if (error !== null) {
-                console.log('exec error: ' + error);
+                res.send(rmsg(2,error,'df操作失败.'));
+                return 
             }
+            var data=[];
+            var item = stdout.toString().split('\n');
+            var title=item[0].split(',');
+            for (var i =1; i <item.length-1; i++) {
+                var disk = item[i].split(',');
+                var disk_data={};
+                disk_data[title[0]]=disk[0];
+                disk_data[title[1]]=disk[1];
+                disk_data[title[2]]=disk[2];
+                disk_data[title[3]]=disk[3];
+                disk_data[title[4]]=disk[4];
+                disk_data[title[5]]=disk[5];
+                // disk_data['Filesystem']=disk[0];
+                // disk_data['1K-blocks']=disk[1];
+                // disk_data['Used']=disk[2];
+                // disk_data['Available']=disk[3];
+                // disk_data['Use%']=disk[4];
+                // disk_data['Mounted_on']=disk[5];
+                // data[i]=disk_data;
+                data.push(disk_data);
+            }            
+            res.send(rmsg(0,data,'获取数据完成'));
+            console.log(data);
         }
-    );
-    res.send(data);
+    );  
 };
